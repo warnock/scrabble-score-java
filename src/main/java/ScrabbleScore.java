8dcd852cd5e9b0804c9d5b3900755efc90d2002b
Megan Warnock and Shane Stafford <megan@>
2017-03-15 16:27:28 -0700

    pass first test

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -12,59 +12,19 @@ public class ScrabbleScore {
   public int calculateScore(String input){
     int totalScore = 0;
     String[] inputArray = input.split("");
-    for (Integer a = 0; a <= inputArray.length; a++) {
-      for (Integer b = 0; b <= letters.length; b++) {
+    for (int a = 0; a < inputArray.length; a++) {
+      for (int b = 0; b < letters.length; b++) {
         if (inputArray[a].equals(letters[b])) {
 
           int index = b;
           totalScore = totalScore + scores[index];
 
-
-
-
           //find the index of the match inside of letters
           //then use that number to .get() from the scores Array
           //add that to total score tally
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          // totalScore = totalScore + scores[0];
-
-
-
-          // Integer index = Integer.parseInt(letters[b]);
-          //
-          // scores.get(index);
-          //
-          // totalScore = totalScore + scoreIndex;  //must change to index of match eventually
-
-
         }
       }
-
-      // if (input.Character.isLetter(i).equals(letterValue1)) {
-      //   i = 1;
-      //   result = i;
-      // }
-
-      // else if (eachLetterValue2.equals(letter)) {
-      //   result = 2;
-      // }
     }
     return totalScore;
   }
